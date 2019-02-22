0bcf5bd5e89f8ce7443049376d63527bf17faefe
Parfait Pascal Sibomana <sasipa90@hotmail.com>
2019-02-22 18:27:07 +0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # wp-starcity-theme
+
+This repository was created for a theme development
+
+## Installation
+
+If you don’t have Docker and Docker Compose installed follow the steps outlined in the blog post linked above.
+
+With Docker installed and running, in Terminal:
+
+````
+git clone https://github.com/psibomana/wp-starcity-theme.git
+cd wp-starcity-theme
+````
+
+Then:
+
+````
+docker-compose up 
+````
+
+Then in your browser:
+````
+http://localhost:8040/
+````
+
+The aforementioned blog post has more information.
---
+++ wp-content/themes/wpstarcitytheme/footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package wpstarcity
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wpstarcity' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'wpstarcity' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wpstarcity' ), 'wpstarcity', '<a href="http://underscores.me/">Underscores.me</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
