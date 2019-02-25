2e59459f33750f53d576683856d9b13b119135da
Parfait Pascal Sibomana <sasipa90@hotmail.com>
2019-02-25 16:41:16 +0300

    Theme footer update

--- footer.php
+++ footer.php
@@ -13,21 +13,88 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wpstarcity' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'wpstarcity' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wpstarcity' ), 'wpstarcity', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	
+  <footer class="section">
+    <div class="mw">
+      <div class="col w-40-l w-100 fl">
+        <a href="/">
+          <img class="footer-logo" src="<?php echo get_template_directory_uri() ?>/assets/svg/logo-wordmark.svg"/>
+        </a>
+      </div>
+      <div class="col w-60-l w-100 fl">
+        <div class="w-25-l w-100 fl">
+          <h6>Join Starcity</h6>
+          <ul class="footer-nav">
+            <li>
+              <a href="/schedule-tour">Schedule a tour</a>
+            </li>
+            <li>
+              <a href="/signup">Apply to move in</a>
+            </li>
+          </ul>
+        </div>
+        <div class="w-25-l w-100 fl">
+          <h6>Why Starcity</h6>
+          <ul class="footer-nav">
+            <li>
+              <a href="/coliving">What is Coliving?</a>
+            </li>
+            <li>
+              <a href="/design">Design</a>
+            </li>
+            <li>
+              <a href="/urban-revival">Urban Revival</a>
+            </li>
+            <li>
+              <a href="/technology">Technology</a>
+            </li>
+            <li>
+              <a href="/families">Starcity for Families</a>
+            </li>
+          </ul>
+        </div>
+        <div class="w-25-l w-100 fl">
+          <h6>About</h6>
+          <ul class="footer-nav">
+            <li>
+              <a href="/company">The Company</a>
+            </li>
+            <li>
+              <a href="/faq">FAQ</a>
+            </li>
+            <li>
+              <a href="https://blog.starcity.com">Blog</a>
+            </li>
+            <li>
+              <a href="/careers">Careers</a>
+            </li>
+            <li>
+              <a href="/press">Press Kit</a>
+            </li>
+          </ul>
+        </div>
+        <div class="w-25-l w-100 fl">
+          <h6>Contact Us</h6>
+          <p>Whether you’re a potential member, partner, or investor, we’d love to hear from you.</p>
+          <a href="/contact" class="button button-outline-white">Get In Touch</a>
+        </div>
+      </div>
+    </div>
+    <div class="mw footer-skyline">
+      <img src="<?php echo get_template_directory_uri() ?>/assets/svg/skyline-sf.svg" />
+    </div>
+    <div class="mw footer-nav-bottom">
+      <div class="w-40 fl">
+        <a href="/terms" class="mr2">Terms</a>
+        <a href="/privacy" class="mr2">Privacy</a>
+      </div>
+      <div class="w-60 fl">
+        <p class="member-login">Already a member?
+          <a href="/login">Log in</a>
+        </p>
+      </div>
+    </div>
+  </footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
